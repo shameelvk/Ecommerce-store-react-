c2842382e38adb990ae211d0d8abdc6e9b7ce7de
shameelvk <shameel3311@gmail.com>
2023-11-16 22:31:17 +0530

    completed iteam and popular components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import hero_image from '../Assets/hero1.png'
 const Hero = () => {
   return (
     <div className='hero'>
@@ -21,10 +21,10 @@ const Hero = () => {
                 <img src={arrow_icon} alt="" />
             </div>
         </div>
-        {/* <div className="hero-right">
+        <div className="hero-right">
             <img src={hero_image} alt="" />
 
-        </div> */}
+        </div>
 
     </div>
   )
